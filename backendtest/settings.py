9428376d0ee599cf9c265dc7c9150c2bf9f41df6
CodeAayu <aayu.16je002440@am.ism.ac.in>
2018-10-15 20:45:24 +0530

    created basic model and first app in web server

--- backendtest/settings.py
+++ backendtest/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'testapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,11 @@ WSGI_APPLICATION = 'backendtest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'theqbackend',
+        'USER': 'root',
+        'PASSWORD': 'Aayush@123',
+        'PORT': '',
     }
 }
 
---
+++ testapp/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class EventData(models.Model):
+	name = models.CharField(max_length=100,null=False)
+	date = models.DateField(null = False)
+	description = models.CharField(max_length=750,null=False)
+	event_id = models.IntegerField(primary_key=True)
+	city = models.CharField(max_length=30)
