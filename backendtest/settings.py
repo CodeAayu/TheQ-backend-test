414d5e02a1526556593e4d639b3b8e4e22771e6b
CodeAayu <aayu.16je002440@am.ism.ac.in>
2018-10-24 14:06:16 +0530

    Updated allowed hosts to all for deployment easibility

--- backendtest/settings.py
+++ backendtest/settings.py
@@ -24,9 +24,7 @@ SECRET_KEY = 'z_13qajl(b=%kfoa$#0_*0p2uzq^k=28gr!#je)%=hrk#_i1o+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
