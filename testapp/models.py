07f2421eeca7996da232f215995aa02bf6a667c2
CodeAayu <aayu.16je002440@am.ism.ac.in>
2018-10-24 13:56:16 +0530

    sending event details as JSON complete

--- testapp/models.py
+++ testapp/models.py
@@ -4,5 +4,5 @@ class EventData(models.Model):
 	name = models.CharField(max_length=100,null=False)
 	date = models.DateField(null = False)
 	description = models.CharField(max_length=750,null=False)
-	event_id = models.IntegerField(primary_key=True)
+	event_id = models.AutoField(primary_key=True)
 	city = models.CharField(max_length=30)
--- testapp/views.py
+++ testapp/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+import datetime , json
+from testapp.models import EventData
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse , JsonResponse
 
-# Create your views here.
+@csrf_exempt
+def savetheevent(request):
+    print (request.body)
+    data = json.loads(request.body)
+    event_name = data["name"]
+    event_date = datetime.datetime.strptime(data["date"],"%d%m%Y").date()
+    event_description = data["description"]
+    event_city = data["city"]
+    EventData.objects.create(name=event_name, date=event_date, description=event_description, city=event_city)
+    return HttpResponse("Event successfully saved!")
+
+@csrf_exempt
+def showeventdetails(request):
+    if request.method == 'GET':
+        data = EventData.objects.all().values()
+        listing = list(data)
+        #print (listing)
+        return JsonResponse(listing, safe=False)
+    return HttpResponse("Wrong request!")
+def deleteevent(request):
+    return HttpResponse("Event successfully saved!")
